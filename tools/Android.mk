d253f5f1a75a33974f30b3f6ba4307dadc387c1f
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-02-24 15:02:48 +0100

    Update sdk up to 8c982e082456b1b252bdb6aa7b39cf0e4d653a94

--- tools/Android.mk
+++ tools/Android.mk
@@ -60,7 +60,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := jack-coverage-plugin
-LOCAL_SRC_FILES := jack-coverage-plugin-STABLE.jar
+LOCAL_SRC_FILES := jack-coverage-plugin.jar
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 MOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
@@ -331,33 +331,4 @@ endif # TARGET_BUILD_APPS only
 
 endif # old version of build/ project.
 
-# Only build Clang/LLVM components when forced to.
-ifneq (true,$(FORCE_BUILD_LLVM_COMPONENTS))
-
-##################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libLLVM
-LOCAL_SRC_FILES := $(HOST_OS)/lib64/$(LOCAL_MODULE)$(HOST_SHLIB_SUFFIX)
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(HOST_SHLIB_SUFFIX)
-LOCAL_IS_HOST_MODULE := true
-LOCAL_MULTILIB := 64
-
-include $(BUILD_PREBUILT)
-
-##################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libclang
-LOCAL_SRC_FILES := $(HOST_OS)/lib64/$(LOCAL_MODULE)$(HOST_SHLIB_SUFFIX)
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(HOST_SHLIB_SUFFIX)
-LOCAL_IS_HOST_MODULE := true
-LOCAL_MULTILIB := 64
-
-include $(BUILD_PREBUILT)
-
-endif #!FORCE_BUILD_LLVM_COMPONENTS
+# libLLVM and libclang are defined in Android.bp
